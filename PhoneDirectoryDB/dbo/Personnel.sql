25dd52cf2ad1e0a3c9f73a94b150d1b32fa1df5b
dsconsole283 <dsconsole_2@yahoo.com>
2022-06-05 14:44:42 -0700

    Add SQLDB project to solution
    Creates SQLDB and tables

---
+++ PhoneDirectoryDB/dbo/Personnel.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Personnel]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [DepartmentId] INT NULL, 
+    [Title] NVARCHAR(50) NOT NULL, 
+    [EmailAddress] NVARCHAR(50) NOT NULL, 
+    [PhoneMain] NCHAR(11) NULL, 
+    [PhoneMobile] NCHAR(11) NULL, 
+    [Extension] NCHAR(5) NULL, 
+    [Notes] NVARCHAR(MAX) NULL, 
+    [IsExec] BIT NOT NULL DEFAULT 0, 
+    CONSTRAINT [FK_Personnel _ToTable] FOREIGN KEY ([DepartmentId]) REFERENCES [Departments]([Id])
+)
