f73df10eab6f94cdcdcf9910dc2288d4a8c2ee5c
dsconsole283 <dsconsole_2@yahoo.com>
2022-06-07 07:17:41 -0700

    Added data annotations to personnel model

--- PhoneDirectory.Library/Models/PersonnelModel.cs
+++ PhoneDirectory.Library/Models/PersonnelModel.cs
@@ -1,19 +1,26 @@
-﻿namespace PhoneDirectory.Library.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhoneDirectory.Library.Models
 {
     public class PersonnelModel
     {
         public int Id{ get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
         public DepartmentModel Department { get; set; }
         public int DepartmentId { get; set; }
+        [Required]
         public TitleModel Title { get; set; }
         public int TitleId { get; set; }
+        [Required]
         public string EmailAddress { get; set; }
         public string PhoneMain { get; set; }
         public string PhoneMobile { get; set; }
         public string Extension { get; set; }
         public string Notes { get; set; }
+        [Required]
         public bool IsExec { get; set; } = false;
     }
 }
