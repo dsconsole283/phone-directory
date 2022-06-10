8d9494ba941e93c7dcff4ab7cf19d04ca1167392
Donnie Console <dsconsole_2@yahoo.com>
2022-06-10 03:08:22 -0700

    Added initial styling to index page

--- PhoneDirectory.Library/DirectoryData.cs
+++ PhoneDirectory.Library/DirectoryData.cs
@@ -13,7 +13,7 @@
         }
         public string GetConnectionString()
         {
-            return _configuration.GetConnectionString("DefaultDataConnection");
+            return _configuration.GetConnectionString("DefaultRemoteDataConnection");
         }
         public async Task AddRecordAsync(PersonnelModel record, string department, string title)
         {
